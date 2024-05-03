753a9de7f6925d15ea5a5f1e659c350f26ea7ff1
MRE <mre@MREs-MBP.lan>
2024-05-04 00:35:06 +0200

    home-page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
-import Image from "next/image";
+import Slider from "@/components/home/slider";
+
 
 
 export default function Home() {
+    return(
+        <>
+        <Slider/>
+        </>
+    )
+
   
 }
