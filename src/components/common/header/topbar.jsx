1d175889e4a4d609a1689bce5fd56d0bb808138e
MRE <mre@MREs-MBP.lan>
2024-05-03 15:03:06 +0200

    topbar

---
+++ src/components/common/header/topbar.jsx
@@ -0,0 +1,20 @@
+"use client"
+import { config } from '@/helpers/config'
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import "./topbar.scss"
+
+const Topbar = () => {
+  return (
+    <div className='topbar'>
+        <Container>
+            <div className='slogan'>
+            🚨 {config.project.slogan}
+            </div>
+            <span>LOGIN</span>
+        </Container>
+    </div>
+  )
+}
+
+export default Topbar
